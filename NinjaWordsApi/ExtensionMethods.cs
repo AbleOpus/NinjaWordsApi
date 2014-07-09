5507326ce3a5224545d5a99c8fd1dff3a1da596d
Brian Ferguson <brianferguson@hotmail.ca>
2014-07-09 14:21:54 -0300

    Removed verbose ext meth and fixed but on TrimEnd

--- NinjaWordsApi/ExtensionMethods.cs
+++ NinjaWordsApi/ExtensionMethods.cs
@@ -16,20 +16,11 @@ namespace NinjaWordsApi
         }
 
         /// <summary>
-        /// Trim all leading and trailing whitespace for each element of the array
-        /// </summary>
-        public static void TrimAll(this string[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-                array[i] = array[i].Trim();
-        }
-
-        /// <summary>
         /// Trims the specified characters from the end of the StringBuilder
         /// </summary>
         public static void TrimEnd(this StringBuilder SB, char c)
         {
-            while (SB[SB.Length - 1] == ',')
+            while (SB[SB.Length - 1] == c)
                 SB.Remove(SB.Length - 1, 1);
         } 
     }
