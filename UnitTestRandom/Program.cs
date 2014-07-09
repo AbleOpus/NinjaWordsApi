c83b8bc3c170e2662e17db0e40501d297e2d02d2
Brian Ferguson <brianferguson@hotmail.ca>
2014-07-09 16:40:04 -0300

    Added unit test

---
+++ UnitTestRandom/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using NinjaWordsApi;
+
+namespace UnitTestRandom
+{
+    /// <summary>
+    /// Throttles the service with random requests
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Increase throttle interval for long tests as to not
+            // get your IP blocked and to be considerate
+            const int INTERVAL = 0;
+            const int LOOKUPS = 500;
+
+            for (int i = 0; i < LOOKUPS; i++)
+            {
+                Thread.Sleep(INTERVAL);
+
+                try
+                {
+                    NinjaTerm term = Ninja.GetRandomTermAsync().Result;
+                    Console.WriteLine((i + 1) + ": " + term.Term);
+                }
+                catch (WebException)
+                {
+                    Console.WriteLine("****************");
+                    Console.WriteLine("WebException");
+                    Console.WriteLine("****************");
+                }
+                catch (CategoryNotEnumeratedException ex)
+                {
+                    Console.WriteLine("****************");
+                    Console.WriteLine("CategoryNotEnumeratedException on category: " + ex.CategoryString);
+                    Console.WriteLine("****************");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
